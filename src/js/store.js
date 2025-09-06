cd8d6ee8ea5213128f4a48250adf75f5f292ee3e
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-09-05 21:41:53 -0400

    feat: AGREGUE PAGINA DE INICIO
    ✅ PAGINA DE INICIO COMPLETAMENTE RESPONSIVE
    ❌ FALTA AGREGAR JS PARA INTERACTUAR CON BOTONES

---
+++ src/js/store.js
@@ -0,0 +1,13 @@
+const carrusel = document.querySelector(".carrusel");
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+document.querySelector(".next").onclick = () => {
+  index = (index + 1) % slides.length;
+  carrusel.scrollTo({ left: slides[index].offsetLeft, behavior: "smooth" });
+};
+
+document.querySelector(".prev").onclick = () => {
+  index = (index - 1 + slides.length) % slides.length;
+  carrusel.scrollTo({ left: slides[index].offsetLeft, behavior: "smooth" });
+};
