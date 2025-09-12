0b4acad572e0da500b7cec806376086170371919
Nek0k4w4N <lucc.martinez@duocuc.cl>
2025-09-12 05:20:55 -0300

    agrege validaciones de login y registro

--- src/js/app.js
+++ src/js/app.js
@@ -37,17 +37,90 @@ document.querySelectorAll(".toggle-password").forEach((btn) => {
   });
 });
 
-mostrarPassword.addEventListener("click", () => {
-  // Alterna el tipo de input
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    togglePassword.classList.replace("fa-eye", "fa-eye-slash");
-  } else {
-    passwordInput.type = "password";
-    togglePassword.classList.replace("fa-eye-slash", "fa-eye");
-  }
-});
-
 // Agregar validaciones
 // Asegurate de no modificar ids ni clases dentro del hmtl.
 // Deje una clase error dentro del css que puedes llamar con {elementoHTML}.classList.add("error").
+
+//formulario de logeo
+const formularioLogin = document.getElementById("formulario-login");
+const correoLogin = document.getElementById("correo-login");
+const claveLogin = document.getElementById("password-login");
+const errorlogin = document.getElementById("error-login");
+
+//validar formato del correo
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+formularioLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //lista errores
+  let mensajeserror = [];
+
+  //validar correo
+  if (!emailRegex.test(correoLogin.value.trim())) {
+    mensajeserror.push("Correo invalido favor revisar.");
+    correoLogin.classList.add("error");
+  } else {
+    correoLogin.classList.remove("error");
+  }
+
+  //validar la contraseña
+  if (claveLogin.value.trim("") === "") {
+    mensajeserror.push("La contraseña no puede estar vacia.");
+    claveLogin.classList.add("error");
+  } else {
+    claveLogin.classList.remove("error");
+  }
+  errorlogin.innerHTML = mensajeserror.join("<br>");
+
+  //evuar login
+  if (mensajeserror.length > 0) {
+  } else {
+    errorlogin.innerHTML = "";
+    alert("login correcto ");
+    formularioLogin.reset();
+  }
+});
+
+//formulario registro
+
+const formularioRegistro = document.getElementById("formulario-crear-cuenta");
+const nombreregistro = document.getElementById("nombre-crear");
+const correoRegistro = document.getElementById("correo-crear");
+const claveregistro = document.getElementById("password-crear");
+const errorregistro = document.getElementById("error-crear");
+
+formularioRegistro.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let mensajeserror = [];
+  //validacion nombre
+  if (!nombreregistro.value.trim()) {
+    mensajeserror.push("Favor revisar el nombre (esta vacio).");
+    nombreregistro.classList.add("error");
+  } else {
+    nombreregistro.classList.remove("error");
+  }
+  //validacion correo
+  if (!emailRegex.test(correoRegistro.value.trim())) {
+    mensajeserror.push("Favor revisar el correo.");
+    correoRegistro.classList.add("error");
+  } else {
+    correoRegistro.classList.remove("error");
+  }
+  //validacion contraseña
+  if (claveregistro.value.trim("") === "") {
+    mensajeserror.push("La contraseña no puede esta vacia.");
+    claveregistro.classList.add("error");
+  } else {
+    claveregistro.classList.remove("error");
+  }
+  //mostrar mensaje de error
+  errorregistro.innerHTML = mensajeserror.join("<br>");
+  //evaluar registro
+  if (mensajeserror.length > 0) {
+  } else {
+    alert("registro correcto");
+    mensajeserror.innerHTML = "";
+    formularioRegistro.reset();
+  }
+});
